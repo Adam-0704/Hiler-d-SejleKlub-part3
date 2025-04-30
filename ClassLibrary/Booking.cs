6cad321b7c44bd6ece0b169eb5ac778c225f4bf2
Patrick-LD <patricklykkedahl@gmail.com>
2025-04-30 10:14:26 +0200

    har lavet boatId til booking class

--- ClassLibrary/Booking.cs
+++ ClassLibrary/Booking.cs
@@ -9,12 +9,14 @@ namespace ClassLibrary
     public class LedigBooking
     {
         public int BookingId { get; set; }
+        public int BoatId { get; set; }
         public DateTime StartDato { get; set; }
         public DateTime SlutDato { get; set; }
 
-        public LedigBooking(int bookingid, DateTime startdato, DateTime slutdato )
+        public LedigBooking(int bookingid, int boatid, DateTime startdato, DateTime slutdato )
         {
             BookingId = bookingid;
+            BoatId = BoatId;
             StartDato = startdato;
             SlutDato = slutdato;
             
